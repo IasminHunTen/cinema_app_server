f8bf57653bf2042e0be86e633d200ae89ab86df3
Admin <constantiniasmin321@yahoo.com>
2022-04-25 16:28:46 +0300

    try/fix some imports issues

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import sys
+
 from main import create_app
 
 if __name__ == '__main__':
+    path = './main'
+    if path not in sys.path:
+        sys.path.append(path)
     app = create_app()
     app.run()
 
